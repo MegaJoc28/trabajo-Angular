a1d6faa14b3010e1d5226bb5018d76902907e044
MegaJoc28 <jzapatasegui@gmail.com>
2025-07-24 18:51:16 -0400

    Creado componente inventario-component. Se agregan todas las tareas solicitadas.

--- src/app/pages/main-page/main-page.ts
+++ src/app/pages/main-page/main-page.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss'
 })
 export class MainPage {
+  constructor(private router: Router) {}
 
+  funcionGuardarRol(rol: string): void {
+    localStorage.setItem('rolUsuario', rol);
+    this.router.navigate(['/inventario']);
+  }
 }
